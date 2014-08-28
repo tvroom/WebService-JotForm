83da2db9a63011669b06f06072f4b9f6049050cd
“Tim <vroom@blockstackers.com>
2014-08-27 21:49:23 -0400

    Updates for v0.006

--- lib/WebService/JotForm.pm
+++ lib/WebService/JotForm.pm
@@ -14,7 +14,7 @@ WebService::JotForm - Perl interface to JotForm's API -- currently only the read
 
 =head1 VERSION
 
-Version 0.005
+Version 0.006
 
 =head1 SYNOPSIS
 	
@@ -51,7 +51,7 @@ More information on tokens is available in the L<JotForm API Documentation|http:
 
 =cut
 
-our $VERSION = '0.005';
+our $VERSION = '0.006';
 
 has 'apiKey'  		=> ( is => 'ro', required => 1);
 has 'apiBase' 		=> ( is => 'ro', default => 'https://api.jotform.com');
