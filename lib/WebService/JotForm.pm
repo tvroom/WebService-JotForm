182feb9c68a10cfa454d868987b84251d0619c4b
Tim Vroom <vroom@blockstackers.com>
2015-03-03 09:10:22 -0500

    Updates for 0.020 release

--- lib/WebService/JotForm.pm
+++ lib/WebService/JotForm.pm
@@ -16,7 +16,7 @@ Support for create, update, and delete operations are beginning to be added in t
 
 =head1 VERSION
 
-Version 0.019
+Version 0.020
 
 =head1 SYNOPSIS
 	
@@ -53,7 +53,7 @@ More information on tokens is available in the L<JotForm API Documentation|http:
 
 =cut
 
-our $VERSION = '0.019';
+our $VERSION = '0.020';
 
 has 'apiKey'  		=> ( is => 'ro', required => 1);
 has 'apiBase' 		=> ( is => 'ro', default => 'https://api.jotform.com');
